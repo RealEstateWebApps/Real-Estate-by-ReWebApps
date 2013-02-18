0128f95853a0d8219ae42987018d46ff921b77fe
bhubbard <bkhubbard@gmail.com>
2013-02-18 12:39:30 -0800

    copy changes for edit gallery

--- modules/properties.php
+++ modules/properties.php
@@ -243,7 +243,7 @@ function prop_show_box() {
 				
 				echo '&emsp; <a href="http://plugin.dev/wp-admin/admin.php?page=nggallery-manage-gallery&mode=edit&gid=' .$prop_meta . '">Edit Gallery</a>';
 				echo     '</td>','</tr>';
-				echo '<tr><td colspan="2"><strong>Note:</strong>&emsp; You must first save or update the property before using the "Edit Gallery" link.</td></tr>';
+				echo '<tr><td colspan="2"><strong>Note:</strong>&emsp; You must first choose a gallery, then save or update the property before using the "Edit Gallery" link.</td></tr>';
 				echo '<tr><td colspan="2"><hr style="background:#ddd; border:0px; height:1px; position:relative; width:100%;" /><h4>Property Price Information</h4></td></tr>';
 			break;
 			case 'price':
